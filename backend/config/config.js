b5c54f4fd47bc56e20b12922176f641da1c19ae3
Mike <ms1822@messiah.edu>
2023-09-03 22:01:35 -0400

    Reorganized project file structure

---
+++ backend/config/config.js
@@ -0,0 +1,3 @@
+// Configuration for app will hold values that we can reuse in the code base
+// This makes it easier to not have hard coded values everywhere and we can
+// refer to this when needed.
