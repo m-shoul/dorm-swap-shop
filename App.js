9c4ef7029fce8a5fac89ca1b7c9dba60c83bcbeb
jp1478 <jp1478@messiah.edu>
2023-09-05 19:48:51 -0400

    Added Logos
    Did something with merge conflicts, not sure if it will cause problems

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>FINALLY WORKING ON THE WEBBBBBB!</Text>
+      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
