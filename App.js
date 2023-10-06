249f63cad66ed3fd56f22af1ebf4e8fe9962adce
jp1478 <112981833+jp1478@users.noreply.github.com>
2023-10-06 16:45:26 -0400

    Revert "Feature/mike task104"

--- App.js
+++ App.js
@@ -5,35 +5,21 @@ import LoginScreen from "./src/screens/LoginScreen";
 import CreateUserScreen from "./src/screens/CreateUserScreen";
 import ResetPasswordScreen from "./src/screens/ResetPasswordScreen";
 import HomeScreen from "./src/screens/HomeScreen";
-import CreatePostScreen from "./src/screens/CreatePostScreen";
-import ChatScreen from "./src/screens/ChatScreen";
-import ProfileScreen from "./src/screens/ProfileScreen";
-import SettingsScreen from "./src/screens/SettingsScreen";
-import ReportScreen from "./src/screens/ReportScreen";
+import useAuth from "./backend/dbFunctions";
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
-return (
+  return (
     <NavigationContainer>
-    {/* changes color based off of current color scheme */}
-    <StatusBar style="auto" />
-    <Stack.Navigator screenOptions={{ headerShown: false }}>
-        {/* Sprint 2 */}
+      {/* changes color based off of current color scheme */}
+      <StatusBar style="auto" />
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
         <Stack.Screen name="LoginScreen" component={LoginScreen} />
         <Stack.Screen name="CreateUserScreen" component={CreateUserScreen} />
         <Stack.Screen name="ResetPasswordScreen" component={ResetPasswordScreen} />
-        
         <Stack.Screen name="HomeScreen" component={HomeScreen} />
-
-        {/* Sprint 3 */} 
-        <Stack.Screen name="CreatePostScreen" component={CreatePostScreen} />
-        <Stack.Screen name="ChatScreen" component={ChatScreen} />
-        <Stack.Screen name="ProfileScreen" component={ProfileScreen} />
-        <Stack.Screen name="SettingsScreen" component={SettingsScreen} />
-        <Stack.Screen name="ReportScreen" component={ReportScreen} />
-        
-    </Stack.Navigator>
+      </Stack.Navigator>
     </NavigationContainer>
-)
+  )
 }
