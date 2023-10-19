25cb096e7ef48aa2f474ed548abce9e8c1f96831
jm1959 <112978130+jm1959@users.noreply.github.com>
2023-10-19 14:39:19 -0400

    Bottom Nav Bar is complete
    Deleted the old nav bar and fully implemented the new on

--- src/components/Component.js
+++ src/components/Component.js
@@ -20,31 +20,35 @@ const Tab = createBottomTabNavigator();
 function BottomTabNavigator() {
     return (
         <Tab.Navigator
-            screenOptions={{ headerShown: false, tabBarStyle: { height: '10%' } }}
+            screenOptions={{
+                headerShown: false, tabBarStyle: { height: '10%' }, tabBarActiveTintColor: "#3F72AF",
+                tabBarInactiveTintColor: "black", tabBarLabelStyle: { fontSize: 13 },
+            }}
+
         >
             <Tab.Screen name="Home" component={HomeScreen} options={{
-                tabBarIcon: () => (
-                    <HomeComponent width="40" height="100" fill="black" strokeWidth="0.25" />
+                tabBarIcon: ({ focused }) => (
+                    <HomeComponent width="40" height="100" fill={focused ? "#3F72AF" : "black"} strokeWidth="0.25" />
                 )
             }} />
             <Tab.Screen name="Chat" component={ChatScreen} options={{
-                tabBarIcon: () => (
-                    <ChatComponent width="40" height="100" fill="black" strokeWidth="0.25" />
+                tabBarIcon: ({ focused }) => (
+                    <ChatComponent width="40" height="100" fill={focused ? "#3F72AF" : "black"} strokeWidth="0.25" />
                 )
             }} />
             <Tab.Screen name="Post" component={CreatePostScreen} options={{
-                tabBarIcon: () => (
-                    <PostComponent width="40" height="100" fill="black" strokeWidth="0.25" />
+                tabBarIcon: ({ focused }) => (
+                    <PostComponent width="40" height="100" fill={focused ? "#3F72AF" : "black"} strokeWidth="0.25" />
                 )
             }} />
             <Tab.Screen name="Profile" component={ProfileScreen} options={{
-                tabBarIcon: () => (
-                    <ProfileComponent width="40" height="100" fill="black" strokeWidth="0.25" />
+                tabBarIcon: ({ focused }) => (
+                    <ProfileComponent width="40" height="100" fill={focused ? "#3F72AF" : "black"} strokeWidth="0.25" />
                 )
             }} />
             <Tab.Screen name="Settings" component={SettingsScreen} options={{
-                tabBarIcon: () => (
-                    <SettingsComponent width="40" height="100" fill="black" strokeWidth="0.25" />
+                tabBarIcon: ({ focused }) => (
+                    <SettingsComponent width="40" height="100" fill={focused ? "#3F72AF" : "black"} strokeWidth="0.25" />
                 )
             }} />
         </Tab.Navigator>
@@ -52,78 +56,3 @@ function BottomTabNavigator() {
 }
 
 export default BottomTabNavigator;
-
-// const NavComponent = ({ navigation }) => {
-//     const [homeColor, setHomeColor] = useState("black");
-//     const [chatColor, setChatColor] = useState("black");
-//     const [postColor, setPostColor] = useState("black");
-//     const [profileColor, setProfileColor] = useState("black");
-//     const [settingColor, setSettingColor] = useState("black");
-//     const switchingPage = async (icon) => {
-//         if (icon == "home") {
-//             setHomeColor("#3F72AF");
-//             setChatColor("black");
-//             setPostColor("black");
-//             setProfileColor("black");
-//             setSettingColor("black");
-//             navigation.navigate('HomeScreen')
-//         }
-//         if (icon == "chat") {
-//             setHomeColor("black");
-//             setChatColor("#3F72AF");
-//             setPostColor("black");
-//             setProfileColor("black");
-//             setSettingColor("black");
-//             navigation.navigate('ChatScreen')
-//         }
-//         if (icon == "post") {
-//             setHomeColor("black");
-//             setChatColor("black");
-//             setPostColor("#3F72AF");
-//             setProfileColor("black");
-//             setSettingColor("black");
-//             navigation.navigate('PostScreen')
-//         }
-//         if (icon == "profile") {
-//             setHomeColor("black");
-//             setChatColor("black");
-//             setPostColor("black");
-//             setProfileColor("#3F72AF");
-//             setSettingColor("black");
-//             navigation.navigate('ProfileScreen')
-//         }
-//         if (icon == "setting") {
-//             setHomeColor("black");
-//             setChatColor("black");
-//             setPostColor("black");
-//             setProfileColor("black");
-//             setSettingColor("#3F72AF");
-//             navigation.navigate('SettingsScreen')
-//         }
-//     }
-//     return (
-//         <View>
-//             <View style={{ backgroundColor: "#B3B3B3", height: "1%", marginLeft: "2%", marginRight: "2%", }} />
-//             {/* Footer */}
-//             <View style={{ flexDirection: 'row', width: '100%', backgroundColor: '#F9F7F7' }}>
-//                 {/*Nav Icons */}
-//                 <TouchableOpacity onPress={() => switchingPage("home")} style={{ marginLeft: '8%' }}>
-//                     <HomeComponent width="40" height="100" fill={homeColor} strokeWidth="0.25" />
-//                 </TouchableOpacity>
-//                 <TouchableOpacity onPress={() => switchingPage("chat")} style={{ marginLeft: '8%' }}>
-//                     <ChatComponent width="40" height='100' fill={chatColor} strokeWidth="0.25" />
-//                 </TouchableOpacity>
-//                 <TouchableOpacity onPress={() => switchingPage("post")} style={{ marginLeft: '8%' }}>
-//                     <PostComponent width="40" height='100' fill={postColor} strokeWidth='0.05' />
-//                 </TouchableOpacity>
-//                 <TouchableOpacity onPress={() => switchingPage("profile")} style={{ marginLeft: '8%' }}>
-//                     <ProfileComponent width="40" height='100' fill={profileColor} strokeWidth='0.05' />
-//                 </TouchableOpacity>
-//                 <TouchableOpacity onPress={() => switchingPage("setting")} style={{ marginLeft: '8%' }}>
-//                     <SettingsComponent width="40" height='100' fill={settingColor} strokeWidth='0.05' />
-//                 </TouchableOpacity>
-//             </View>
-//         </View>
-//     );
-// }
-// export default NavComponent;
