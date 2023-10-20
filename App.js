0c355485f9c276a713f67d3c5e1d8d9efeb449ea
Mike <ms1822@messiah.edu>
2023-10-20 12:15:00 -0400

    Updated App.js for stable build

--- App.js
+++ App.js
@@ -5,14 +5,15 @@ import LoginScreen from "./src/screens/LoginScreen";
 import CreateUserScreen from "./src/screens/CreateUserScreen";
 import ResetPasswordScreen from "./src/screens/ResetPasswordScreen";
 // import HomeScreen from "./src/screens/HomeScreen";
-// import CreatePostScreen from "./src/screens/CreatePostScreen";
-// import ChatScreen from "./src/screens/ChatScreen";
-// import ProfileScreen from "./src/screens/ProfileScreen";
-// import SettingsScreen from "./src/screens/SettingsScreen";
-// import ReportScreen from "./src/screens/ReportScreen";
-//import PostReportedScreen from "./src/screens/PostReportedScreen";
+import CreatePostScreen from "./src/screens/CreatePostScreen";
+import ChatScreen from "./src/screens/ChatScreen";
+import ProfileScreen from "./src/screens/ProfileScreen";
+import SettingsScreen from "./src/screens/SettingsScreen";
+import ReportScreen from "./src/screens/ReportScreen";
+import PostReportedScreen from "./src/screens/PostReportedScreen";
 //import BottomTabNavigator from "./src/components/Components.js";
 import BottomTabNavigator from "./src/components/Component";
+import ListingPopup from "./src/components/ListingPopup";
 
 
 const Stack = createStackNavigator();
@@ -30,11 +31,14 @@ export default function App() {
                 <Stack.Screen name="HomeScreen" component={BottomTabNavigator} />
 
                 {/* Sprint 3 */}
-                {/* <Stack.Screen name="CreatePostScreen" component={CreatePostScreen} />
+                <Stack.Screen name="CreatePostScreen" component={CreatePostScreen} />
+                <Stack.Screen name="ListingPopup" component={ListingPopup} />
                 <Stack.Screen name="ChatScreen" component={ChatScreen} />
                 <Stack.Screen name="ProfileScreen" component={ProfileScreen} />
                 <Stack.Screen name="SettingsScreen" component={SettingsScreen} />
-                <Stack.Screen name="ReportScreen" component={ReportScreen} /> */}
+                <Stack.Screen name="ReportScreen" component={ReportScreen} />
+                <Stack.Screen name="PostReportedScreen" component={PostReportedScreen} />
+                
 
             </Stack.Navigator>
         </NavigationContainer>
