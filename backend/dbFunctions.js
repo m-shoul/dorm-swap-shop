7284f68cf0e8223926a1f7c408a92ba2bd6bfdd9
jp1478 <jp1478@messiah.edu>
2023-11-07 19:17:03 -0500

    delete old comments

--- backend/dbFunctions.js
+++ backend/dbFunctions.js
@@ -61,57 +61,6 @@ export const getUserID = () => {
 
 }
 
-// // Write user data to the database.
-// export function writeUserData(fname, lname, uname, email) {
-
-//     // Reference users in database
-//     const userReference = ref(database, 'dorm_swap_shop/users/');
-
-//     // Generates a unique ID
-//     const newUserReference = push(userReference);
-
-//     // Gets the unique ID
-//     const userId = newUserReference.key;
-
-//     const userData = {
-//         fname: fname,
-//         lname: lname,
-//         username: uname,
-//         email: email,
-//         // profile_picture : imageUrl
-//     };
-
-//     set(newUserReference, userData);
-
-//     return userId;
-// }
-
-// // Write post data to the database.
-// export function writeListingData(title, description, price, userId) {
-
-//     // Reference listings in database
-//     const listingReference = ref(database, 'dorm_swap_shop/listings/');
-
-//     // Generates a unique ID
-//     const newListingReference = push(listingReference);
-
-//     // Gets the unique ID
-//     const listingId = newListingReference.key;
-
-//     const listingData = {
-//         title: title,
-//         description: description,
-//         price: price,
-//         userId: userId,
-//         // timeUpload: firebase.database.ServerValue.TIMESTAMP
-//         timeUpload: Date.now()
-//     };
-
-//     set(newListingReference, listingData);
-
-//     return listingId;
-// }
-
     
 
     
