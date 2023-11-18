468e5195a37c59a89d095b6fcc55e9cc8a83189a
Mike <ms1822@messiah.edu>
2023-11-18 11:17:33 -0500

    Fixed SafeAreaProvider tag in ListingPopup

--- components/ListingPopup.js
+++ components/ListingPopup.js
@@ -243,6 +243,6 @@ export default function ListingPopup({ listing }) {
                     </View>
                 </SafeAreaView>
             </Modal>
-        </SafeAreaView>
+        </SafeAreaProvider>
     );
 }
