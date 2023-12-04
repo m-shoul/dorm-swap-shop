4a145b801023a74213965e1229803e35fa82e4a4
jm1959 <112978130+jm1959@users.noreply.github.com>
2023-12-04 15:52:03 -0500

    Fixed the stying 0 error

--- app/(aux)/StyleSheet.js
+++ app/(aux)/StyleSheet.js
@@ -180,9 +180,9 @@ const styles = StyleSheet.create({
 
     searchContainer: {
         position: "absolute",
-        top: "0",
-        left: "0",
-        right: "0",
+        top: 0,
+        left: 0,
+        right: 0,
         backgroundColor: "transparent",
         borderBottomColor: "transparent",
         borderTopColor: "transparent",
