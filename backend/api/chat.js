208d48f522e8c4bc4eb5ca9df30c9d335beb2d97
jp1478 <jp1478@messiah.edu>
2024-01-12 20:55:05 -0500

    chat api upload

--- backend/api/chat.js
+++ backend/api/chat.js
@@ -1,17 +1,71 @@
-import { database } from './config/firebaseConfig';
+import { database } from '../config/firebaseConfig.js';
 import { get, child, ref, set, push, getDatabase } from 'firebase/database';
 
 // ^^ Import whatever we need for this...
 // NOTE************ add additional parameters when needed!!! This is just a baseline.
 
 // Function to create a new user
-export function createChat(chatData) {
+export async function createChatThread(userId_1, userId_2) {
     // Implement the functionality to create a chat between users in the db.
     // Think about this and how EACH user will have a different list of
     // chats depending on who they reply to.
 
     // The "Reply", button will most likely call this method and then
     // navigate to the chat screen.
+
+    const chatReference = ref(database, 'dorm_swap_shop/chats');
+
+    const newChatReference = push(chatReference);
+
+    const chatId = newChatReference.key;
+
+    const chatData = {
+        chatId: chatId,
+        participants: {
+            [userId_1]: {
+                messages: []
+            },
+            [userId_2]: {
+                messages: []
+            }
+        },
+        reported: false
+    };
+
+    await set(newChatReference, chatData); 
+
+    console.log("Chat created with id: " + chatId);
+
+    return chatId;
+}
+
+export async function addMessage(chatId, userId, message) {
+    try {
+        const chatRef = ref(database, `dorm_swap_shop/chats/${chatId}`);
+        const snapshot = await get(chatRef);
+
+        if (!snapshot.exists()) {
+            // await createChatThread(userId, userId_2);
+            throw new Error('Chat does not exist.');
+        }
+
+        const messageData = {
+            message: message,
+            timestamp: new Date().toISOString(),
+        };
+
+        const messagesRef = ref(database, `dorm_swap_shop/chats/${chatId}/participants/${userId}/messages`);
+        const newMessageReference = push(messagesRef);
+
+        await set(newMessageReference, messageData);
+
+        console.log("Message added to chat: " + chatId + ": " + message);
+
+        return newMessageReference.key;
+    } catch (error) {
+        console.error('Error adding message:', error);
+        throw error;
+    }
 }
 
 // Function to read user data
