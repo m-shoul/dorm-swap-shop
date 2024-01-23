510ab48c35091c7bb8a87268d0e5d2d8dcf94f10
jp1478 <jp1478@messiah.edu>
2024-01-23 18:51:46 -0500

    Chat Screen
    added library functionality, still in progress

--- app/(chat)/ConversationsScreen.js
+++ app/(chat)/ConversationsScreen.js
@@ -8,16 +8,49 @@ import {
 } from "react-native";
 import styles from "../(aux)/StyleSheet";
 import { router } from "expo-router";
+import { database } from "../../backend/firebase";
+import React, { useState, useCallback, useEffect } from 'react'
+import { GiftedChat } from 'react-native-gifted-chat'
 
 export default function Conversations() {
-    return (
-        <SafeAreaView style={styles.background}>
-            <View>
-                <Text style={styles.resetHeader}>Conversations</Text>
-            </View>
-            <TouchableOpacity onPress={() => router.push("(home)/Home")}>
-                <Text>Home</Text>
-            </TouchableOpacity>
-        </SafeAreaView>
-    );
-};
+  const [messages, setMessages] = useState([])
+
+  useEffect(() => {
+    setMessages([
+      {
+        _id: 1,
+        text: 'Hello developer',
+        createdAt: new Date(),
+        user: {
+          _id: 2,
+          name: 'React Native',
+          avatar: 'https://placeimg.com/140/140/any',
+        },
+      },
+    ])
+  }, [])
+
+  const onSend = useCallback((messages = []) => {
+    setMessages(previousMessages =>
+      GiftedChat.append(previousMessages, messages),
+    )
+  }, [])
+
+  return (
+    // <SafeAreaView style={styles.background}>
+    // <View>
+    //     <Text style={styles.resetHeader}>Conversations</Text>
+    // </View>
+    // <TouchableOpacity onPress={() => router.push("(home)/Home")}>
+    //     <Text>Home</Text>
+    // </TouchableOpacity>
+    <GiftedChat
+        messages={messages}
+        onSend={messages => onSend(messages)}
+        user={{
+        _id: 1,
+        }}
+    />
+    // </SafeAreaView>
+  )
+}
