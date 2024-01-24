d3c2e3553d76da6938876c27f0a403b5a0262e35
BenOfTheOneRing <bc1335@messiah.edu>
2024-01-23 19:46:49 -0500

    fixed listings being cut off in the home page.

--- app/(home)/Home.js
+++ app/(home)/Home.js
@@ -4,22 +4,18 @@ import {
     TouchableOpacity,
     FlatList,
     SafeAreaView,
-    //Image,
     Animated,
     RefreshControl,
-    ActivityIndicator
+    ActivityIndicator,
 } from "react-native";
-import { Image } from 'expo-image';
+import { Image } from "expo-image";
 import React, { useState, useEffect, useRef, useMemo } from "react";
 import { getAllListings } from "../../backend/api/listing";
 import styles from "../(aux)/StyleSheet";
 import ListingPopup from "../../components/ListingPopup";
 import { ScrollView } from "react-native-web";
-//import styles from "../styleSheets/StyleSheet.js";
-//import { HeaderComponent } from "../components/headerComponent.js";
 import filter from "lodash.filter";
 import typescript from "react-native-svg";
-// import { get, child, ref, set, push } from "firebase/database";
 import SearchBarHeader from "../../components/SearchBar";
 import { getUsernameByID } from "../../backend/api/user";
 
@@ -56,12 +52,12 @@ export default function HomeScreen() {
                 setIsLoading(false);
             } catch (error) {
                 setError(error);
-                console.error('Error:', error);
+                console.error("Error:", error);
                 setRefreshing(false);
                 setIsLoading(false);
             }
         }, 1000); // Delay of 1 second
-    }
+    };
 
     useEffect(() => {
         // Fetch listings data from Firebase when the component mounts
@@ -72,36 +68,54 @@ export default function HomeScreen() {
         };
     }, []);
 
-    const memoizedListingsData = useMemo(() => Object.values(listingsData), [listingsData]);
+    const memoizedListingsData = useMemo(
+        () => Object.values(listingsData),
+        [listingsData]
+    );
 
     console.log("***IN APP - Home.js*** - Printing out listings.");
     console.log(memoizedListingsData);
 
     const handleSearch = async (query) => {
-        if (typeof fullData !== 'object') {
-            console.error('fullData is not an object:', fullData);
+        if (typeof fullData !== "object") {
+            console.error("fullData is not an object:", fullData);
             return;
         }
         const formattedQuery = query.toLowerCase();
-        const filteredData = await Promise.all(Object.values(fullData).map(async (listing) => {
-            const username = await getUsernameByID(listing.user);
-            if (username && contains(listing, formattedQuery, username.toLowerCase())) {
-                return listing;
-            }
-        }));
+        const filteredData = await Promise.all(
+            Object.values(fullData).map(async (listing) => {
+                const username = await getUsernameByID(listing.user);
+                if (
+                    username &&
+                    contains(listing, formattedQuery, username.toLowerCase())
+                ) {
+                    return listing;
+                }
+            })
+        );
         setListingsData(filteredData.filter(Boolean)); // Remove undefined values
-    }
+    };
 
-    const contains = ({ title, description, condition, category}, query, username) => {
+    const contains = (
+        { title, description, condition, category },
+        query,
+        username
+    ) => {
         title = title.toLowerCase();
         description = description.toLowerCase();
         category = category.toLowerCase();
-    
-        if (title.includes(query) || description.includes(query) || condition.includes(query) || username.includes(query) || category.includes(query)) {
+
+        if (
+            title.includes(query) ||
+            description.includes(query) ||
+            condition.includes(query) ||
+            username.includes(query) ||
+            category.includes(query)
+        ) {
             return true;
         }
         return false;
-    }
+    };
 
     const handleItemPress = (listing) => {
         setSelectedListing(listing);
@@ -128,20 +142,31 @@ export default function HomeScreen() {
 
     return (
         <SafeAreaView style={{ flex: 1, backgroundColor: "#F9F7F7" }}>
-            <SearchBarHeader animHeaderValue={scrollOffsetY} handleSearch={handleSearch} />
+            <SearchBarHeader
+                animHeaderValue={scrollOffsetY}
+                handleSearch={handleSearch}
+            />
             {/* Scrollable view displaying all the listings */}
             <FlatList
                 data={Object.values(memoizedListingsData)}
                 keyExtractor={(item) => item.listingId}
                 renderItem={({ item }) => (
-                    <View style={{ width: "50%", height: 230, padding: "1%" }}>
+                    <View
+                        style={{
+                            width: "50%",
+                            height: 230,
+                            padding: "1%",
+                        }}>
                         <ListingPopup
                             listing={item}
-                        //navigation={router}
+                            //navigation={router}
                         />
                     </View>
                 )}
                 numColumns={2}
+                contentContainerStyle={{
+                    paddingBottom: "15%", // Add this line
+                }}
                 style={{
                     flex: 1,
                     backgroundColor: "#F9F7F7",
